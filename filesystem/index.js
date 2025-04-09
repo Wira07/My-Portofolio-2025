6c3543440a0ab9fe66298068ee4410670f26ee2e
wiral <wiralodrasaputra07@gmail.com>
2025-04-09 20:42:58 +0700

    Add filesystem module

---
+++ filesystem/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+ 
+const fileReadCallback = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas');
+        return; 
+    }
+    console.log(data);
+};
+ 
+const data = fs.readFileSync('notes.txt', 'UTF-8');
+
+fs.readFile('notes.txt  ', 'UTF-8', fileReadCallback);
+console.log(data);
